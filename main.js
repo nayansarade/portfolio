fa2d7787e397ca13472c5b97ee8f4c79564db1bc
Nayan Sarade <121233354+nayansarade@users.noreply.github.com>
2023-09-02 22:48:38 +0530

    Add files via upload
    Commit

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new typed(".text", {
+    Strings:["Frontend Developer" , "Youtube" , "Web Developer"],
+    typespeed: 100,
+    backspeed: 100,
+    backDelay: 1000,
+    loop: true
+});
